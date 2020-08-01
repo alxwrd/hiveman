fa2f0c3095f4e5a3be519bf7d885d4fe342238fa
Alex Ward <alxwrd@googlemail.com>
2020-08-01 22:28:21 +0100

    hide login when authed

--- src/App.svelte
+++ src/App.svelte
@@ -1,7 +1,17 @@
 <script>
   import Login from "./components/Login.svelte";
+
+  import { token } from "./stores.js";
+
+  let authed = false;
+  const unsub = token.subscribe(value => {
+    authed = value ? true : false;
+  });
 </script>
 
-<div class="container mx-auto my-16 xl:my-32 flex justify-center items-center">
-  <Login></Login>
-</div>
+{#if !authed}
+  <div
+    class="container mx-auto my-16 xl:my-32 flex justify-center items-center">
+    <Login />
+  </div>
+{/if}
