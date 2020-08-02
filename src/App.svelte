7bcfb2ea0ab83854101cd37e4c6cd647b14b02f6
Alex Ward <alxwrd@googlemail.com>
2020-08-02 18:09:48 +0100

    add nodes, allow to be turned on/off

--- src/App.svelte
+++ src/App.svelte
@@ -1,5 +1,22 @@
 <script>
   import Header from "./components/Header.svelte";
+  import Device from "./components/Device.svelte";
+
+  import * as api from "./api.js";
+
+  window.api = api;
+
+  let nodes = [];
+  api.nodes().then(data => {
+    nodes = data;
+  });
+
 </script>
 
 <Header />
+
+<div class="flex flex-wrap">
+{#each nodes as node}
+  <Device node={node} />
+{/each}
+</div>
---
+++ src/components/Device.svelte
@@ -0,0 +1,31 @@
+<script>
+  import * as api from "../api.js";
+
+  export let node;
+
+  function turnOn() {
+    api.turnOn(node.id);
+  }
+
+  function turnOff() {
+    api.turnOff(node.id);
+  }
+</script>
+
+<div id={node.id} class="w-64 h-64 md:w-64 m-4 p-4 mx-auto bg-yellow-100 rounded shadow">
+  <div class="">
+    <p class="text-lg pb-2 font-medium">{node.name}</p>
+    <button
+      class="bg-yellow-500 hover:bg-yellow-700 text-white font-bold py-2 px-4
+      rounded"
+      on:click={turnOn}>
+      ON
+    </button>
+    <button
+      class="bg-yellow-500 hover:bg-yellow-700 text-white font-bold py-2 px-4
+      rounded"
+      on:click={turnOff}>
+      OFF
+    </button>
+  </div>
+</div>
