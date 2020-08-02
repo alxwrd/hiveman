a75e8eb57fabf544030e9c84b94e24f5da155719
Alex Ward <alxwrd@googlemail.com>
2020-08-02 16:09:44 +0100

    add page header

---
+++ src/components/Header.svelte
@@ -0,0 +1,37 @@
+<script>
+  import { auth } from "../stores.js";
+
+  import Logo from "./Logo.svelte";
+
+  let authDetails = {};
+  let userHash = "";
+  const unsub = auth.subscribe(value => {
+    authDetails = value;
+    userHash = md5(authDetails.username || "");
+    console.log(userHash);
+  });
+</script>
+
+{#if authDetails.username}
+  <header class="bg-yellow-600 h-16 flex justify-between shadow-md">
+    <div class="w-1/4 md:w-1/6 lg:w-1/12" />
+    <div class="w-1/2">
+      <Logo />
+    </div>
+    <div
+      class="w-1/4 md:w-1/6 lg:w-1/12"
+      on:click={() => {
+        auth.set({});
+      }}>
+      <div class="py-4 md:py-2">
+        <img
+          class="h-8 md:h-12 mx-6 absolute right-0 rounded-full shadow-md
+          hover:scale-105 transform transition-transform ease-in-out
+          duration-500"
+          src="https://www.gravatar.com/avatar/{userHash}?d=identicon"
+          alt="User avatar" />
+      </div>
+    </div>
+
+  </header>
+{/if}
